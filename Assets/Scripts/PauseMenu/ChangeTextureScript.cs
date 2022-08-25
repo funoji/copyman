283e4b980860241867691fce73788b3e06200f19
kusomanju <g2127072@st.t-kougei.ac.jp>
2022-08-25 23:22:17 +0900

    ポーズ画面の微調整
    UIの差し替えなどを行いました。

---
+++ Assets/Scripts/PauseMenu/ChangeTextureScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTextureScript : MonoBehaviour
+{
+    [SerializeField] Sprite DefaultTexture;
+    [SerializeField] Sprite ChangeTexture;
+    void Start()
+    {
+        this.gameObject.GetComponent<Image>().sprite=DefaultTexture;
+    }
+
+    private void OnMouseOver()
+    {
+        this.gameObject.GetComponent<Image>().sprite=ChangeTexture;
+    }
+
+    private void OnMouseExit()
+    {
+        this.gameObject.GetComponent<Image>().sprite=DefaultTexture;
+    }
+}
