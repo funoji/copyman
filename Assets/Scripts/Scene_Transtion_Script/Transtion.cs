04bdbb4f80a4d0db52c5951be9d4812571f100a4
ZakiAKR <zakichan.ak@gmail.com>
2022-11-06 21:05:42 +0900

    フェードアウトの修正

--- Assets/Scripts/Scene_Transtion_Script/Transtion.cs
+++ Assets/Scripts/Scene_Transtion_Script/Transtion.cs
@@ -5,43 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Transtion : MonoBehaviour
 {
-    public void Trans_Menu()
-    {
-        SceneManager.LoadScene("S-02_Menu_Scene");
-    }
+    public void Trans_ToMenu() { SceneManager.LoadScene("S-02_Menu_Scene"); }
 
-    public void Trans_StageSelect()
-    {
-        SceneManager.LoadScene("Stage_01");
-    }
+    public void Trans_ToGameEnd() { Application.Quit(); }
 
-    public void Trans_Option()
-    {
-        SceneManager.LoadScene("S-03_Option_Scene");
-    }
+    public void Trans_ToOption() { SceneManager.LoadScene("S-03_Option_Scene"); }
 
-    public void Trans_Catalog()
-    {
-        SceneManager.LoadScene("Stage_01");
-    }
+    public void Trans_ToCatalog() { /*SceneManager.LoadScene(" ");*/ }
 
-    public void Trasn_RetuenTitle()
-    {
-        SceneManager.LoadScene("S-01_Title_Scene");
-    }
+    public void Trasn_ToRetuenTitle() { SceneManager.LoadScene("S-01_Title_Scene"); }
 
-    public void Trans_GameClear()
-    {
-        SceneManager.LoadScene("S-01_Title_Scene");
-    }
+    public void Trans_ToStage1() { /*SceneManager.LoadScene("Stage1");*/ }
 
-    public void Trans_GameOver()
-    {
-        SceneManager.LoadScene("GameOverScene");
-    }
-
-    public void Trans_GameEnd()
-    {
-        Application.Quit();
-    }
+    public void Trans_ToStage4() { SceneManager.LoadScene("Stage4"); }
+    
+    public void Trans_ToSelect() { /*SceneManager.LoadScene(" ");*/ }
 }
