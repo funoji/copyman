486dd42ba92133f99962556f654ba48502c4e5ad
megutan30 <g2127064@st.t-kougei.ac.jp>
2022-11-06 20:58:09 +0900

    更新

---
+++ Assets/VFX/Megitan_SoundManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Megitan_SoundManager : MonoBehaviour
+{
+    private static Megitan_SoundManager instance;
+    private AudioSource Sound;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Sound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        SoundVolume();     
+    }
+
+    private void SoundVolume()
+    {
+        if ((Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.UpArrow)) || (Input.GetKeyDown(KeyCode.Joystick1Button3) && Input.GetKey(KeyCode.Joystick1Button9)))
+        {
+            Debug.Log("VolumeUp");
+            Sound.volume += 0.1f;
+        }
+
+        if ((Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.DownArrow)) || (Input.GetKeyDown(KeyCode.Joystick1Button0) && Input.GetKey(KeyCode.Joystick1Button9)))
+        {
+            Debug.Log("VolumeDown");
+            Sound.volume -= 0.1f;
+        }
+    }
+}
