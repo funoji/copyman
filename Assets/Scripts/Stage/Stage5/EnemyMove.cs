8c5a2a8ba4b219adb2864a96f497d346f77523f4
megutan30 <g2127064@st.t-kougei.ac.jp>
2022-11-21 23:10:44 +0900

    Stage5更新_001

--- Assets/Scripts/Stage/Stage5/EnemyMove.cs
+++ Assets/Scripts/Stage/Stage5/EnemyMove.cs
@@ -4,25 +4,155 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1.0f;
 
-    private CharacterController enemyController;
-    private Vector3 destination;
-    private Vector3 velocity;
+    private bool isStan;
+    private bool isAttck;
+    private bool arrived;
+    private Vector3 prePos;
     private Vector3 direction;
+    private Vector3 startPosition;
+    private Vector3 destination;
+    private float elapsedTime;
+
+    [SerializeField] private float searchDis;
+    [SerializeField] private GameObject attackObj;
+    [SerializeField] private float attackSpd;
+    [SerializeField] private float moveSpd;
+    [SerializeField] private float maxSpd;
+    [SerializeField] private float waitTime = 5f;
+
+    public bool ActiveGimmick
+    {
+        get => isAttck;
+        set => isAttck = value;
+    }
+    public bool ActiveStan
+    {
+        get => isStan;
+        set => isStan = value;
+    }
 
     void Start()
     {
-        enemyController = GetComponent<CharacterController>();
-        destination = new Vector3(0f, 0f, 0f);
-        velocity = Vector3.zero;
+        prePos = transform.position;
+        isAttck = false;
+        isStan = false;
+        arrived = false;
+        elapsedTime = 0f;
+        startPosition = transform.position;
+        SetDestination(transform.position);
     }
 
+    private void FixedUpdate()
+    {
+        if (isStan) return;
+        Move();
+        Attack();
+        RotateToMove();
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude <= 5)
+        {
+            IsStan();
+            Invoke("IsStan", 3.0f);
+            Debug.Log("aaaa");
+        }
 
-    // Update is called once per frame
-    void Update()
+        if(collision.gameObject.tag == "Player")
+        {
+            GameDirector.GameOver = true;
+        }
+    }
+
+    void Move()
+    {
+        if (isAttck) return;
+        if (!arrived)
+        {
+            direction = (destination - transform.position).normalized;
+
+            transform.Translate(direction * moveSpd * Time.deltaTime,Space.World);
+
+            //目的地に到着したかどうかの判定
+            if (Vector3.Distance(transform.position, destination) < 0.5f)
+            {
+                arrived = true;
+            }
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+
+            //待ち時間を越えたら次の目的地を設定
+            if (elapsedTime > waitTime)
+            {
+                CreateRandomPosition();
+                destination = GetDestination();
+                arrived = false;
+                elapsedTime = 0f;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        IsAttack();
+        if (!isAttck) return;
+
+        Vector3 posA = attackObj.transform.position;
+        Vector3 posB = transform.position;
+        Vector3 dif = (posA - posB).normalized * attackSpd;
+        Vector3 attackVec = new Vector3(dif.x, 0, dif.z);
+
+        transform.Translate(attackVec * Time.deltaTime,Space.World);
+        
+    }
+
+    void RotateToMove()
     {
         
+        Vector3 diff = transform.position - prePos;
+        prePos = transform.position;
+        if (diff.magnitude <= 0.01) return;
+        transform.rotation = Quaternion.LookRotation(diff,Vector3.up);
+    }
+
+    void IsAttack()
+    {
+        float dis = Vector3.Distance(attackObj.transform.position, transform.position);
+        if (dis < searchDis)
+        {
+            isAttck = true;
+            return;
+        }
+        isAttck = false;
+    }
+
+    void IsStan()
+    {
+        if (isStan)
+        {
+            isStan = false; 
+            return;
+        }
+        if (!isStan) isStan = true;
+    }
+
+    public void CreateRandomPosition()
+    {
+        var randDestination = Random.insideUnitCircle * 8;
+        SetDestination(startPosition + new Vector3(randDestination.x, 0, randDestination.y));
+    }
+
+    public void SetDestination(Vector3 position)
+    {
+        destination = position;
+    }
+
+    public Vector3 GetDestination()
+    {
+        return destination;
     }
 }
