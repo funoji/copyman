a1f3856aba3f4f7dd93ae32a9d2958998487aae1
funoji <90616260+funoji@users.noreply.github.com>
2022-12-04 00:49:54 +0900

    アニメーション完成！！

--- Assets/Scripts/Camera_Script/Animator_Controller.cs
+++ Assets/Scripts/Camera_Script/Animator_Controller.cs
@@ -7,16 +7,39 @@ public class Animator_Controller : MonoBehaviour
     [Header("Animator Setting")]
     [SerializeField] [Tooltip("Debug_Camera_Animator Reference")] Animator animator;
     [SerializeField] [Tooltip("PlayerController Reference")] PlayerController moveController;
+    [SerializeField] JumpManager JumpManager;
+    Rigidbody rb;
+    float state = 0.2f;
 
     void Awake()
     {
         TryGetComponent(out animator);
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveController.horizontal > 0.2f || moveController.vertical > 0.2f || moveController.horizontal < -0.2f || moveController.vertical < -0.2f) animator.SetBool("isRun", true);
+        Debug.Log(rb.velocity);
+        if (moveController.horizontal > state ||
+            moveController.vertical > state ||
+            moveController.horizontal < -state ||
+            moveController.vertical < -state)
+        {
+            animator.SetBool("isRun", true);
+        }
         else animator.SetBool("isRun", false);
+
+        if (rb.velocity.y > state)
+        {
+            animator.SetBool("isJump", true);
+        }
+        else animator.SetBool("isJump", false);
+
+        if (rb.velocity.y < -state)
+        {
+            animator.SetBool("isFall", true);
+        }
+        else animator.SetBool("isFall", false);
     }
 }
