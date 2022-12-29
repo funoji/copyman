b9a8df13059cd22d23b60f1736dfad91afb5936c
megutan30 <g2127064@st.t-kougei.ac.jp>
2022-12-29 20:50:10 +0900

    更新

--- Assets/Scripts/Stage/Stage3/Stage3_GameManager.cs
+++ Assets/Scripts/Stage/Stage3/Stage3_GameManager.cs
@@ -5,28 +5,54 @@ using UnityEngine;
 public class Stage3_GameManager : MonoBehaviour
 {
     [SerializeField] float onElevatorCount;
+    [SerializeField] float spawnBomNum;
+    [SerializeField] GameObject boms;
+    [SerializeField] Transform spawnPos;
     [SerializeField] Stage3_CrearFlag stage3_CrearFlag;
     [SerializeField] MoveGimmick[] elevator;
 
     private bool onElevetor = false;
+    private bool isactive = true;
+
     void Update()
     { 
-        JudgeCount();
         OnElevator();
     }
 
     void OnElevator()
     {
+        if (!isactive) return;
+        JudgeCount();
         if (!onElevetor) return;
+
         for(int i = 0;i<elevator.Length;i++)
         {
             elevator[i].ActiveGimmick = true;
         }
+
+        StartCoroutine(SpawnBoms());
+        isactive = false;
     }
 
     void JudgeCount()
     {
-        if (stage3_CrearFlag.pushButtonNum >= onElevatorCount) onElevetor = true;
+        if (stage3_CrearFlag.pushButtonNum >= onElevatorCount) onElevetor =true;
+    }
+
+    IEnumerator SpawnBoms()
+    {
+        for (int i = 0; i < spawnBomNum; i++)
+        {
+            GameObject bom = Instantiate(boms, spawnPos);
+            Rigidbody rb = bom.GetComponent<Rigidbody>();
+
+            bom.transform.position = spawnPos.position;
+            bom.name = boms.name;
+            rb.AddForce(new Vector3(100f, 0, 0), ForceMode.Acceleration);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+        StopCoroutine(SpawnBoms());
     }
 
 }
