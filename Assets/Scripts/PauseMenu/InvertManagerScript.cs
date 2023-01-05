ed1016ad641beeb89dc8b0c0ea1419156a2636dd
megutan30 <g2127064@st.t-kougei.ac.jp>
2023-01-05 21:09:29 +0900

    インバート実装

--- Assets/Scripts/PauseMenu/InvertManagerScript.cs
+++ Assets/Scripts/PauseMenu/InvertManagerScript.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class InvertManagerScript : MonoBehaviour
 {
     public static bool HoriInvert = false;
     public static bool VertInvert = false;
 
+    CinemachineVirtualCamera cinemaCamera;
+    private void Start()
+    {
+        cinemaCamera = GameObject.Find("CameraSystem").GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(cinemaCamera.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_HorizontalAxis.m_InvertInput);
+    }
+
     public void HoriInvertIsCheck()
     {
         HoriInvert = !HoriInvert;
+        cinemaCamera.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_HorizontalAxis.m_InvertInput =
+            !cinemaCamera.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_HorizontalAxis.m_InvertInput;
     }
     public void VertInvertIsCheck()
     {
         VertInvert = !VertInvert;
+        cinemaCamera.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_VerticalAxis.m_InvertInput =
+            !cinemaCamera.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_VerticalAxis.m_InvertInput;
     }
 }
